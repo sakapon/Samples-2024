48c37012e5826d61dbe9a865f6f1afe71c1e2e65
Keiho Sakapon <git@saka-pon.net>
2024-02-29 22:49:11 +0900

    Add classes

---
+++ AlgorithmSample/AlgorithmLib10/SegTrees/SegTrees104/Monoid.cs
@@ -0,0 +1,22 @@
+﻿
+namespace AlgorithmLib10.SegTrees.SegTrees104
+{
+	public static class Monoid
+	{
+		public static Monoid<int> Int32_Add { get; } = new Monoid<int>((x, y) => x + y);
+		public static Monoid<long> Int64_Add { get; } = new Monoid<long>((x, y) => x + y);
+		public static Monoid<int> Int32_Min { get; } = new Monoid<int>((x, y) => x <= y ? x : y, int.MaxValue);
+		public static Monoid<int> Int32_Max { get; } = new Monoid<int>((x, y) => x >= y ? x : y, int.MinValue);
+
+		public static Monoid<int> Int32_ArgMin(int[] a) => new Monoid<int>((x, y) => a[x] <= a[y] ? x : y);
+
+		public static Monoid<int> Int32_Update { get; } = new Monoid<int>((x, y) => x != int.MinValue ? x : y, int.MinValue);
+	}
+
+	public struct Monoid<T>
+	{
+		public Func<T, T, T> Op;
+		public T Id;
+		public Monoid(Func<T, T, T> op, T id = default(T)) { Op = op; Id = id; }
+	}
+}
---
+++ AlgorithmSample/OnlineTest10/SegTrees/Test04/Merge_ABC339_E.cs
@@ -0,0 +1,27 @@
+﻿using AlgorithmLib10.SegTrees.SegTrees104;
+
+namespace OnlineTest10.SegTrees.Test04
+{
+	// Test: https://atcoder.jp/contests/abc339/tasks/abc339_e
+	class Merge_ABC339_E
+	{
+		static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
+		static void Main() => Console.WriteLine(Solve());
+		static object Solve()
+		{
+			var (n, d) = Read2();
+			var a = Read();
+
+			var monoid = new Monoid<int>((x, y) => x >= y ? x : y, 0);
+			var st = new Int32MergeTree<int>(monoid);
+
+			foreach (var v in a)
+			{
+				var nv = st[v - d, v + d + 1] + 1;
+				if (st[v] < nv) st[v] = nv;
+			}
+			return st[0, 1 << 30];
+		}
+	}
+}
