7480b37956cef84e6e546cdff8d529d3f4b8b9a9
Keiho Sakapon <git@saka-pon.net>
2024-03-02 01:19:37 +0900

    Update code

--- AlgorithmSample/AlgorithmLib10/SegTrees/SegTrees104/Int32RSQTree.cs
+++ AlgorithmSample/AlgorithmLib10/SegTrees/SegTrees104/Int32RSQTree.cs
@@ -10,8 +10,8 @@ namespace AlgorithmLib10.SegTrees.SegTrees104
 			public Node Left, Right;
 		}
 
-		// [-1 << MaxDigit, 1 << MaxDigit)
-		const int MaxDigit = 30;
+		// [Min, Max)
+		const int MinIndex = -1 << 30, MaxIndex = 1 << 30;
 		Node Root;
 		readonly List<Node> Path = new List<Node>();
 
@@ -28,58 +28,72 @@ namespace AlgorithmLib10.SegTrees.SegTrees104
 
 		public long Get(int l, int r)
 		{
-			ScanNodes(l, r);
-			var v = 0L;
-			foreach (var n in Path) v += n.Value;
-			return v;
+			if (l < MinIndex) l = MinIndex;
+			if (r > MaxIndex) r = MaxIndex;
+			return Get(Root, MinIndex, MaxIndex, l, r);
 		}
 
 		public void Add(int key, long value)
 		{
-			ScanOrAddNode(key);
+			GetOrAddNode(key);
 			foreach (var n in Path) n.Value += value;
 		}
 
-		void ScanNodes(int l, int r)
+		long Get(Node node, int nl, int nr, int l, int r)
 		{
-			Path.Clear();
-			ScanNodes(Root, -1 << MaxDigit, 1 << MaxDigit, l, r);
-		}
-
-		void ScanNodes(Node node, int nl, int nr, int l, int r)
-		{
-			if (node == null) return;
-			if (nl == l && nr == r) { Path.Add(node); return; }
+			if (node == null) return 0;
+			if (nl == l && nr == r) return node.Value;
 			var nc = nl + nr >> 1;
-			if (l < nc) ScanNodes(node.Left, nl, nc, l, nc < r ? nc : r);
-			if (nc < r) ScanNodes(node.Right, nc, nr, l < nc ? nc : l, r);
+			var v = l < nc ? Get(node.Left, nl, nc, l, nc < r ? nc : r) : 0;
+			return nc < r ? v + Get(node.Right, nc, nr, l < nc ? nc : l, r) : v;
 		}
 
 		Node GetNode(int key)
 		{
-			throw new NotImplementedException();
+			var node = Root;
+			var (nl, nr) = (MinIndex, MaxIndex);
+			while (true)
+			{
+				if (node == null) break;
+				if (nl + 1 == nr) break;
+				var nc = nl + nr >> 1;
+				if (key < nc)
+				{
+					node = node.Left;
+					nr = nc;
+				}
+				else
+				{
+					node = node.Right;
+					nl = nc;
+				}
+			}
+			return node;
 		}
 
-		void ScanOrAddNode(int key)
+		Node GetOrAddNode(int key)
 		{
 			Path.Clear();
 			ref var node = ref Root;
-			var nc = 0;
-			for (int d = MaxDigit - 1; d >= -2; --d)
+			var (nl, nr) = (MinIndex, MaxIndex);
+			while (true)
 			{
 				node ??= new Node();
 				Path.Add(node);
+				if (nl + 1 == nr) break;
+				var nc = nl + nr >> 1;
 				if (key < nc)
 				{
 					node = ref node.Left;
-					nc -= 1 << d;
+					nr = nc;
 				}
 				else
 				{
 					node = ref node.Right;
-					nc |= 1 << d;
+					nl = nc;
 				}
 			}
+			return node;
 		}
 	}
 }
