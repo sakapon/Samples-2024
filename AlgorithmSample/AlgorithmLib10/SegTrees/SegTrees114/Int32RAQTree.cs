0ca35760c08f3764acbf977e2b68feb30fe81c75
Keiho Sakapon <git@saka-pon.net>
2024-03-03 01:08:46 +0900

    Update code

--- AlgorithmSample/AlgorithmLib10/SegTrees/SegTrees114/Int32RAQTree.cs
+++ AlgorithmSample/AlgorithmLib10/SegTrees/SegTrees114/Int32RAQTree.cs
@@ -3,86 +3,70 @@ namespace AlgorithmLib10.SegTrees.SegTrees114
 {
 	public class Int32RAQTree
 	{
-		// [-1 << MaxDigit, 1 << MaxDigit)
-		const int MaxDigit = 30;
+		// [MinIndex, MaxIndex)
+		const int MinIndex = -1 << 30, MaxIndex = 1 << 30;
 		long[] values;
-		int[] l, r;
+		int[] ln, rn;
 		int t;
 		int Root;
-		readonly List<int> Path = new List<int>();
 
-		public Int32RAQTree(int size = 1 << 22)
-		{
-			Initialize(size);
-		}
-
-		public void Clear()
-		{
-			Initialize(l.Length);
-		}
-
+		public Int32RAQTree(int size = 1 << 22) => Initialize(size);
+		public void Clear() => Initialize(values.Length);
 		void Initialize(int size)
 		{
 			values = new long[size];
-			l = new int[size];
-			r = new int[size];
-			Array.Fill(l, -1);
-			Array.Fill(r, -1);
+			ln = new int[size];
+			rn = new int[size];
+			Array.Fill(ln, -1);
+			Array.Fill(rn, -1);
 			t = 0;
 		}
 
-		public long this[int key]
+		public long this[int key] => Get(key);
+
+		public void Add(int l, int r, long value)
 		{
-			get
+			if (l < MinIndex) l = MinIndex;
+			if (r > MaxIndex) r = MaxIndex;
+			Add(ref Root, MinIndex, MaxIndex, l, r, value);
+		}
+
+		void Add(ref int node, int nl, int nr, int l, int r, long value)
+		{
+			if (node == -1) node = ++t;
+			if (nl == l && nr == r) { values[node] += value; return; }
+			var nc = nl + nr >> 1;
+			if (l < nc) Add(ref ln[node], nl, nc, l, nc < r ? nc : r, value);
+			if (nc < r) Add(ref rn[node], nc, nr, l < nc ? nc : l, r, value);
+		}
+
+		public long Get(int key)
+		{
+			var v = 0L;
+			var node = Root;
+			var (nl, nr) = (MinIndex, MaxIndex);
+			while (true)
 			{
-				ScanNode(key);
-				var v = 0L;
-				foreach (var n in Path) v += values[n];
-				return v;
+				if (node == -1) return v;
+				v += values[node];
+				if (nl + 1 == nr) return v;
+				var nc = nl + nr >> 1;
+				if (key < nc) { nr = nc; node = ln[node]; }
+				else { nl = nc; node = rn[node]; }
 			}
 		}
 
-		public void Add(int key, long value) => Add(key, key + 1, value);
-		public void Add(int l, int r, long value)
+		public void Add(int key, long value)
 		{
-			AddNode(l, r);
-			foreach (var n in Path) values[n] += value;
-		}
-
-		void AddNode(int l, int r)
-		{
-			Path.Clear();
-			AddNode(ref Root, -1 << MaxDigit, 1 << MaxDigit, l, r);
-		}
-
-		void AddNode(ref int node, int nl, int nr, int l, int r)
-		{
-			if (node == -1) node = ++t;
-			if (nl == l && nr == r) { Path.Add(node); return; }
-			var nc = (nl + nr) >> 1;
-			if (l < nc) AddNode(ref this.l[node], nl, nc, l, nc < r ? nc : r);
-			if (nc < r) AddNode(ref this.r[node], nc, nr, l < nc ? nc : l, r);
-		}
-
-		void ScanNode(int key)
-		{
-			Path.Clear();
-			var node = Root;
-			var nc = 0;
-			for (int d = MaxDigit - 1; d >= -2; --d)
+			ref var node = ref Root;
+			var (nl, nr) = (MinIndex, MaxIndex);
+			while (true)
 			{
-				if (node == -1) return;
-				Path.Add(node);
-				if (key < nc)
-				{
-					node = l[node];
-					nc -= 1 << d;
-				}
-				else
-				{
-					node = r[node];
-					nc |= 1 << d;
-				}
+				if (node == -1) node = ++t;
+				if (nl + 1 == nr) { values[node] += value; return; }
+				var nc = nl + nr >> 1;
+				if (key < nc) { nr = nc; node = ref ln[node]; }
+				else { nl = nc; node = ref rn[node]; }
 			}
 		}
 	}
--- AlgorithmSample/AlgorithmLib10/SegTrees/SegTrees114/Int32RSQTree.cs
+++ AlgorithmSample/AlgorithmLib10/SegTrees/SegTrees114/Int32RSQTree.cs
@@ -3,86 +3,70 @@ namespace AlgorithmLib10.SegTrees.SegTrees114
 {
 	public class Int32RSQTree
 	{
-		// [-1 << MaxDigit, 1 << MaxDigit)
-		const int MaxDigit = 30;
+		// [MinIndex, MaxIndex)
+		const int MinIndex = -1 << 30, MaxIndex = 1 << 30;
 		long[] values;
-		int[] l, r;
+		int[] ln, rn;
 		int t;
 		int Root;
-		readonly List<int> Path = new List<int>();
 
-		public Int32RSQTree(int size = 1 << 22)
-		{
-			Initialize(size);
-		}
-
-		public void Clear()
-		{
-			Initialize(l.Length);
-		}
-
+		public Int32RSQTree(int size = 1 << 22) => Initialize(size);
+		public void Clear() => Initialize(values.Length);
 		void Initialize(int size)
 		{
 			values = new long[size];
-			l = new int[size];
-			r = new int[size];
-			Array.Fill(l, -1);
-			Array.Fill(r, -1);
+			ln = new int[size];
+			rn = new int[size];
+			Array.Fill(ln, -1);
+			Array.Fill(rn, -1);
 			t = 0;
 		}
 
-		public long this[int key] => this[key, key + 1];
-		public long this[int l, int r]
+		public long this[int key] => Get(key);
+		public long this[int l, int r] => Get(l, r);
+
+		public long Get(int l, int r)
 		{
-			get
+			if (l < MinIndex) l = MinIndex;
+			if (r > MaxIndex) r = MaxIndex;
+			return Get(Root, MinIndex, MaxIndex, l, r);
+		}
+
+		long Get(int node, int nl, int nr, int l, int r)
+		{
+			if (node == -1) return 0;
+			if (nl == l && nr == r) return values[node];
+			var nc = nl + nr >> 1;
+			var v = l < nc ? Get(ln[node], nl, nc, l, nc < r ? nc : r) : 0;
+			return nc < r ? v + Get(rn[node], nc, nr, l < nc ? nc : l, r) : v;
+		}
+
+		public long Get(int key)
+		{
+			var node = Root;
+			var (nl, nr) = (MinIndex, MaxIndex);
+			while (true)
 			{
-				ScanNode(l, r);
-				var v = 0L;
-				foreach (var n in Path) v += values[n];
-				return v;
+				if (node == -1) return 0;
+				if (nl + 1 == nr) return values[node];
+				var nc = nl + nr >> 1;
+				if (key < nc) { nr = nc; node = ln[node]; }
+				else { nl = nc; node = rn[node]; }
 			}
 		}
 
 		public void Add(int key, long value)
 		{
-			AddNode(key);
-			foreach (var n in Path) values[n] += value;
-		}
-
-		void ScanNode(int l, int r)
-		{
-			Path.Clear();
-			ScanNode(Root, -1 << MaxDigit, 1 << MaxDigit, l, r);
-		}
-
-		void ScanNode(int node, int nl, int nr, int l, int r)
-		{
-			if (node == -1) return;
-			if (nl == l && nr == r) { Path.Add(node); return; }
-			var nc = (nl + nr) >> 1;
-			if (l < nc) ScanNode(this.l[node], nl, nc, l, nc < r ? nc : r);
-			if (nc < r) ScanNode(this.r[node], nc, nr, l < nc ? nc : l, r);
-		}
-
-		void AddNode(int key)
-		{
-			Path.Clear();
 			ref var node = ref Root;
-			var nc = 0;
-			for (int d = MaxDigit - 1; d >= -2; --d)
+			var (nl, nr) = (MinIndex, MaxIndex);
+			while (true)
 			{
 				if (node == -1) node = ++t;
-				Path.Add(node);
-				if (key < nc)
-				{
-					node = ref l[node];
-					nc -= 1 << d;
-				}
-				else
-				{
-					node = ref r[node];
-					nc |= 1 << d;
-				}
+				values[node] += value;
+				if (nl + 1 == nr) return;
+				var nc = nl + nr >> 1;
+				if (key < nc) { nr = nc; node = ref ln[node]; }
+				else { nl = nc; node = ref rn[node]; }
 			}
 		}
 	}
