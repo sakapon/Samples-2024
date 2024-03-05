d77e86ca004dabe0848042d794cb910c12e4f983
Keiho Sakapon <git@saka-pon.net>
2024-03-06 02:18:01 +0900

    Implement 204, 214

--- AlgorithmSample/AlgorithmLib10/SegTrees/SegTrees204/Int32RAQTree.cs
+++ AlgorithmSample/AlgorithmLib10/SegTrees/SegTrees204/Int32RAQTree.cs
@@ -3,15 +3,16 @@ namespace AlgorithmLib10.SegTrees.SegTrees204
 {
 	public class Int32RAQTree
 	{
-		[System.Diagnostics.DebuggerDisplay(@"Value = {Value}")]
+		[System.Diagnostics.DebuggerDisplay(@"[{L}, {R}), Value = {Value}")]
 		public class Node
 		{
-			public long Value;
+			public int L, R;
 			public Node Left, Right;
+			public long Value;
 		}
 
 		// [MinIndex, MaxIndex)
-		const int MinIndex = -1 << 30, MaxIndex = 1 << 30;
+		const int MinIndex = 0, MaxIndex = 1 << 30;
 		Node Root;
 
 		public void Clear() => Root = null;
@@ -22,46 +23,102 @@ namespace AlgorithmLib10.SegTrees.SegTrees204
 		{
 			if (l < MinIndex) l = MinIndex;
 			if (r > MaxIndex) r = MaxIndex;
-			Add(ref Root, MinIndex, MaxIndex, l, r, value);
+			Add(ref Root, l, r, value);
 		}
 
-		void Add(ref Node node, int nl, int nr, int l, int r, long value)
+		void Add(ref Node node, int l, int r, long value)
 		{
-			node ??= new Node();
-			if (nl == l && nr == r) { node.Value += value; return; }
-			var nc = nl + nr >> 1;
-			if (l < nc) Add(ref node.Left, nl, nc, l, nc < r ? nc : r, value);
-			if (nc < r) Add(ref node.Right, nc, nr, l < nc ? nc : l, r, value);
+			if (node == null)
+			{
+				if (l + 1 == r)
+				{
+					node = new Node { L = l, R = r, Value = value };
+					return;
+				}
+				else
+				{
+					var f = MaxBit(l ^ (r - 1));
+					var nl = l & ~(f | (f - 1));
+					node = new Node { L = nl, R = nl + (f << 1) };
+				}
+			}
+
+			if (node.L == l && r == node.R) { node.Value += value; return; }
+
+			if (!(node.L <= l && r <= node.R))
+			{
+				var child = node;
+				var nl = node.L < l ? node.L : l;
+				var f = MaxBit(nl ^ (node.R > r ? node.R - 1 : r - 1));
+				nl &= ~(f | (f - 1));
+				node = new Node { L = nl, R = nl + (f << 1) };
+				(child.L < (nl | f) ? ref node.Left : ref node.Right) = child;
+			}
+
+			var nc = (node.L + node.R) >> 1;
+			if (l < nc) Add(ref node.Left, l, nc < r ? nc : r, value);
+			if (nc < r) Add(ref node.Right, l < nc ? nc : l, r, value);
 		}
 
 		public long Get(int key)
 		{
 			var v = 0L;
 			var node = Root;
-			var (nl, nr) = (MinIndex, MaxIndex);
 			while (true)
 			{
 				if (node == null) return v;
+				if (!(node.L <= key && key < node.R)) return v;
 				v += node.Value;
-				if (nl + 1 == nr) return v;
-				var nc = nl + nr >> 1;
-				if (key < nc) { nr = nc; node = node.Left; }
-				else { nl = nc; node = node.Right; }
+				if (key == node.L && key + 1 == node.R) return v;
+				var nc = node.L + node.R >> 1;
+				node = key < nc ? node.Left : node.Right;
 			}
 		}
 
 		public void Add(int key, long value)
 		{
 			ref var node = ref Root;
-			var (nl, nr) = (MinIndex, MaxIndex);
 			while (true)
 			{
-				node ??= new Node();
-				if (nl + 1 == nr) { node.Value += value; return; }
-				var nc = nl + nr >> 1;
-				if (key < nc) { nr = nc; node = ref node.Left; }
-				else { nl = nc; node = ref node.Right; }
+				if (node == null)
+				{
+					node = new Node { L = key, R = key + 1, Value = value };
+					return;
+				}
+				else if (node.L <= key && key < node.R)
+				{
+					if (key == node.L && key + 1 == node.R) { node.Value += value; return; }
+					var nc = node.L + node.R >> 1;
+					node = ref (key < nc ? ref node.Left : ref node.Right);
+				}
+				else
+				{
+					var child = node;
+					var f = MaxBit(node.L ^ key);
+					var l = key & ~(f | (f - 1));
+					node = new Node { L = l, R = l + (f << 1) };
+					if (child.L < (l | f))
+					{
+						node.Left = child;
+						node = ref node.Right;
+					}
+					else
+					{
+						node.Right = child;
+						node = ref node.Left;
+					}
+				}
 			}
+		}
+
+		static int MaxBit(int x)
+		{
+			x |= x >> 1;
+			x |= x >> 2;
+			x |= x >> 4;
+			x |= x >> 8;
+			x |= x >> 16;
+			return x ^ (x >> 1);
 		}
 	}
 }
--- AlgorithmSample/AlgorithmLib10/SegTrees/SegTrees214/Int32RAQTree.cs
+++ AlgorithmSample/AlgorithmLib10/SegTrees/SegTrees214/Int32RAQTree.cs
@@ -4,22 +4,25 @@ namespace AlgorithmLib10.SegTrees.SegTrees214
 	public class Int32RAQTree
 	{
 		// [MinIndex, MaxIndex)
-		const int MinIndex = -1 << 30, MaxIndex = 1 << 30;
-		long[] values;
+		const int MinIndex = 0, MaxIndex = 1 << 30;
+		int[] li, ri;
 		int[] ln, rn;
-		int t;
-		int Root;
+		long[] values;
+		int Root, t;
 
 		public Int32RAQTree(int size = 1 << 22) => Initialize(size);
 		public void Clear() => Initialize(values.Length);
 		void Initialize(int size)
 		{
-			values = new long[size];
+			li = new int[size];
+			ri = new int[size];
 			ln = new int[size];
 			rn = new int[size];
+			values = new long[size];
+
 			Array.Fill(ln, -1);
 			Array.Fill(rn, -1);
-			t = 0;
+			Root = t = -1;
 		}
 
 		public long this[int key] => Get(key);
@@ -28,46 +31,114 @@ namespace AlgorithmLib10.SegTrees.SegTrees214
 		{
 			if (l < MinIndex) l = MinIndex;
 			if (r > MaxIndex) r = MaxIndex;
-			Add(ref Root, MinIndex, MaxIndex, l, r, value);
+			Add(ref Root, l, r, value);
 		}
 
-		void Add(ref int node, int nl, int nr, int l, int r, long value)
+		void Add(ref int node, int l, int r, long value)
 		{
-			if (node == -1) node = ++t;
-			if (nl == l && nr == r) { values[node] += value; return; }
-			var nc = nl + nr >> 1;
-			if (l < nc) Add(ref ln[node], nl, nc, l, nc < r ? nc : r, value);
-			if (nc < r) Add(ref rn[node], nc, nr, l < nc ? nc : l, r, value);
+			if (node == -1)
+			{
+				if (l + 1 == r)
+				{
+					node = ++t;
+					li[node] = l;
+					ri[node] = r;
+					values[node] = value;
+					return;
+				}
+				else
+				{
+					var f = MaxBit(l ^ (r - 1));
+					var nl = l & ~(f | (f - 1));
+					node = ++t;
+					li[node] = nl;
+					ri[node] = nl + (f << 1);
+				}
+			}
+
+			if (li[node] == l && r == ri[node]) { values[node] += value; return; }
+
+			if (!(li[node] <= l && r <= ri[node]))
+			{
+				var child = node;
+				var nl = li[node] < l ? li[node] : l;
+				var f = MaxBit(nl ^ (ri[node] > r ? ri[node] - 1 : r - 1));
+				nl &= ~(f | (f - 1));
+				node = ++t;
+				li[node] = nl;
+				ri[node] = nl + (f << 1);
+				(li[child] < (nl | f) ? ref ln[node] : ref rn[node]) = child;
+			}
+
+			var nc = li[node] + ri[node] >> 1;
+			if (l < nc) Add(ref ln[node], l, nc < r ? nc : r, value);
+			if (nc < r) Add(ref rn[node], l < nc ? nc : l, r, value);
 		}
 
 		public long Get(int key)
 		{
 			var v = 0L;
 			var node = Root;
-			var (nl, nr) = (MinIndex, MaxIndex);
 			while (true)
 			{
 				if (node == -1) return v;
+				if (!(li[node] <= key && key < ri[node])) return v;
 				v += values[node];
-				if (nl + 1 == nr) return v;
-				var nc = nl + nr >> 1;
-				if (key < nc) { nr = nc; node = ln[node]; }
-				else { nl = nc; node = rn[node]; }
+				if (key == li[node] && key + 1 == ri[node]) return v;
+				var nc = li[node] + ri[node] >> 1;
+				node = key < nc ? ln[node] : rn[node];
 			}
 		}
 
 		public void Add(int key, long value)
 		{
 			ref var node = ref Root;
-			var (nl, nr) = (MinIndex, MaxIndex);
 			while (true)
 			{
-				if (node == -1) node = ++t;
-				if (nl + 1 == nr) { values[node] += value; return; }
-				var nc = nl + nr >> 1;
-				if (key < nc) { nr = nc; node = ref ln[node]; }
-				else { nl = nc; node = ref rn[node]; }
+				if (node == -1)
+				{
+					node = ++t;
+					li[node] = key;
+					ri[node] = key + 1;
+					values[node] = value;
+					return;
+				}
+				else if (li[node] <= key && key < ri[node])
+				{
+					if (key == li[node] && key + 1 == ri[node]) { values[node] += value; return; }
+					var nc = li[node] + ri[node] >> 1;
+					node = ref (key < nc ? ref ln[node] : ref rn[node]);
+				}
+				else
+				{
+					var child = node;
+					var f = MaxBit(li[node] ^ key);
+					var l = key & ~(f | (f - 1));
+					node = ++t;
+					li[node] = l;
+					ri[node] = l + (f << 1);
+					if (li[child] < (l | f))
+					{
+						ln[node] = child;
+						node = ref rn[node];
+					}
+					else
+					{
+						rn[node] = child;
+						node = ref ln[node];
+					}
+				}
 			}
+		}
+
+		static int MaxBit(int x)
+		{
+			x |= x >> 1;
+			x |= x >> 2;
+			x |= x >> 4;
+			x |= x >> 8;
+			x |= x >> 16;
+			return x ^ (x >> 1);
 		}
 	}
 }
