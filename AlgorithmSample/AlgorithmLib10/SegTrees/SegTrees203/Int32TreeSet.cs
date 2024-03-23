fd7ef0b078a4877d5db70a0d35a71a1453967535
Keiho Sakapon <git@saka-pon.net>
2024-03-23 17:18:02 +0900

    Add abstract class

--- AlgorithmSample/AlgorithmLib10/SegTrees/SegTrees203/Int32TreeSet.cs
+++ AlgorithmSample/AlgorithmLib10/SegTrees/SegTrees203/Int32TreeSet.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace AlgorithmLib10.SegTrees.SegTrees203
 {
-	public class Int32TreeSet
+	public abstract class Int32TreeSetBase
 	{
 		[System.Diagnostics.DebuggerDisplay(@"[{L}, {R}), Count = {Count}")]
 		public class Node
@@ -12,14 +12,14 @@ namespace AlgorithmLib10.SegTrees.SegTrees203
 		}
 
 		// [MinIndex, MaxIndex)
-		const int MinIndex = 0;
-		const int MaxIndex = 1 << 30;
-		Node Root;
+		public const int MinIndex = 0;
+		public const int MaxIndex = 1 << 30;
+		protected Node Root;
 		public long Count => Root != null ? Root.Count : 0;
 
 		public void Clear() => Root = null;
 
-		static int MaxBit(int x)
+		protected static int MaxBit(int x)
 		{
 			x |= x >> 1;
 			x |= x >> 2;
@@ -27,86 +27,6 @@ namespace AlgorithmLib10.SegTrees.SegTrees203
 			x |= x >> 8;
 			x |= x >> 16;
 			return x ^ (x >> 1);
-		}
-
-		public bool Add(int key)
-		{
-			return Add(ref Root, key);
-
-			bool Add(ref Node node, int key)
-			{
-				if (node == null)
-				{
-					node = new Node { L = key, R = key + 1, Count = 1 };
-					return true;
-				}
-				else if (key == node.L && key + 1 == node.R)
-				{
-					if (node.Count != 0) return false;
-					node.Count = 1;
-					return true;
-				}
-				else if (node.L <= key && key < node.R)
-				{
-					var nc = node.L + node.R >> 1;
-					var b = Add(ref key < nc ? ref node.Left : ref node.Right, key);
-					if (b) ++node.Count;
-					return b;
-				}
-				else
-				{
-					var child = node;
-					var f = MaxBit(node.L ^ key);
-					var l = key & ~(f | (f - 1));
-					node = new Node { L = l, R = l + (f << 1), Count = child.Count + 1 };
-					if (child.L < (l | f))
-					{
-						node.Left = child;
-						Add(ref node.Right, key);
-					}
-					else
-					{
-						node.Right = child;
-						Add(ref node.Left, key);
-					}
-					return true;
-				}
-			}
-		}
-
-		public bool Remove(int key)
-		{
-			return Remove(ref Root, key);
-
-			bool Remove(ref Node node, int key)
-			{
-				if (node == null) return false;
-				if (key == node.L && key + 1 == node.R)
-				{
-					if (node.Count == 0) return false;
-					node.Count = 0;
-					return true;
-				}
-				if (!(node.L <= key && key < node.R)) return false;
-				var nc = node.L + node.R >> 1;
-				var b = Remove(ref key < nc ? ref node.Left : ref node.Right, key);
-				if (b) --node.Count;
-				return b;
-			}
-		}
-
-		public bool Contains(int key)
-		{
-			return Get(Root, key);
-
-			bool Get(Node node, int key)
-			{
-				if (node == null) return false;
-				if (key == node.L && key + 1 == node.R) return node.Count != 0;
-				if (!(node.L <= key && key < node.R)) return false;
-				var nc = node.L + node.R >> 1;
-				return Get(key < nc ? node.Left : node.Right, key);
-			}
 		}
 
 		public long GetCount(int l, int r)
@@ -188,6 +108,89 @@ namespace AlgorithmLib10.SegTrees.SegTrees203
 		public int RemoveLast() => RemoveAt(Count - 1);
 		public int RemoveFirstGeq(int key) => RemoveAt(GetFirstIndexGeq(key));
 		public int RemoveLastLeq(int key) => RemoveAt(GetLastIndexLeq(key));
+	}
+
+	public class Int32TreeSet : Int32TreeSetBase
+	{
+		public bool Add(int key)
+		{
+			return Add(ref Root, key);
+
+			bool Add(ref Node node, int key)
+			{
+				if (node == null)
+				{
+					node = new Node { L = key, R = key + 1, Count = 1 };
+					return true;
+				}
+				else if (key == node.L && key + 1 == node.R)
+				{
+					if (node.Count != 0) return false;
+					node.Count = 1;
+					return true;
+				}
+				else if (node.L <= key && key < node.R)
+				{
+					var nc = node.L + node.R >> 1;
+					var b = Add(ref key < nc ? ref node.Left : ref node.Right, key);
+					if (b) ++node.Count;
+					return b;
+				}
+				else
+				{
+					var child = node;
+					var f = MaxBit(node.L ^ key);
+					var l = key & ~(f | (f - 1));
+					node = new Node { L = l, R = l + (f << 1), Count = child.Count + 1 };
+					if (child.L < (l | f))
+					{
+						node.Left = child;
+						Add(ref node.Right, key);
+					}
+					else
+					{
+						node.Right = child;
+						Add(ref node.Left, key);
+					}
+					return true;
+				}
+			}
+		}
+
+		public bool Remove(int key)
+		{
+			return Remove(ref Root, key);
+
+			bool Remove(ref Node node, int key)
+			{
+				if (node == null) return false;
+				if (key == node.L && key + 1 == node.R)
+				{
+					if (node.Count == 0) return false;
+					node.Count = 0;
+					return true;
+				}
+				if (!(node.L <= key && key < node.R)) return false;
+				var nc = node.L + node.R >> 1;
+				var b = Remove(ref key < nc ? ref node.Left : ref node.Right, key);
+				if (b) --node.Count;
+				return b;
+			}
+		}
+
+		public bool Contains(int key)
+		{
+			return Get(Root, key);
+
+			bool Get(Node node, int key)
+			{
+				if (node == null) return false;
+				if (key == node.L && key + 1 == node.R) return node.Count != 0;
+				if (!(node.L <= key && key < node.R)) return false;
+				var nc = node.L + node.R >> 1;
+				return Get(key < nc ? node.Left : node.Right, key);
+			}
+		}
 
 		public int[] ToArray()
 		{
@@ -202,6 +205,103 @@ namespace AlgorithmLib10.SegTrees.SegTrees203
 				if (node.Left == null && node.Right == null)
 				{
 					if (node.Count != 0) r[++i] = node.L;
+					return;
+				}
+				Get(node.Left);
+				Get(node.Right);
+			}
+		}
+	}
+
+	public class Int32TreeMultiSet : Int32TreeSetBase
+	{
+		public void Add(int key, long value = 1)
+		{
+			Add(ref Root, key, value);
+
+			void Add(ref Node node, int key, long value)
+			{
+				if (node == null)
+				{
+					node = new Node { L = key, R = key + 1, Count = value };
+				}
+				else if (key == node.L && key + 1 == node.R)
+				{
+					node.Count += value;
+				}
+				else if (node.L <= key && key < node.R)
+				{
+					node.Count += value;
+					var nc = node.L + node.R >> 1;
+					Add(ref key < nc ? ref node.Left : ref node.Right, key, value);
+				}
+				else
+				{
+					var child = node;
+					var f = MaxBit(node.L ^ key);
+					var l = key & ~(f | (f - 1));
+					node = new Node { L = l, R = l + (f << 1), Count = child.Count + value };
+					if (child.L < (l | f))
+					{
+						node.Left = child;
+						Add(ref node.Right, key, value);
+					}
+					else
+					{
+						node.Right = child;
+						Add(ref node.Left, key, value);
+					}
+				}
+			}
+		}
+
+		public long GetCount(int key)
+		{
+			return Get(Root, key);
+
+			long Get(Node node, int key)
+			{
+				if (node == null) return 0;
+				if (key == node.L && key + 1 == node.R) return node.Count;
+				if (!(node.L <= key && key < node.R)) return 0;
+				var nc = node.L + node.R >> 1;
+				return Get(key < nc ? node.Left : node.Right, key);
+			}
+		}
+
+		public int[] ToArray()
+		{
+			var r = new int[Count];
+			var i = -1;
+			Get(Root);
+			return r;
+
+			void Get(Node node)
+			{
+				if (node == null) return;
+				if (node.Left == null && node.Right == null)
+				{
+					var c = node.Count;
+					while (c-- > 0) r[++i] = node.L;
+					return;
+				}
+				Get(node.Left);
+				Get(node.Right);
+			}
+		}
+
+		public (int key, long count)[] ToKeyCountArray()
+		{
+			var r = new List<(int, long)>();
+			Get(Root);
+			return r.ToArray();
+
+			void Get(Node node)
+			{
+				if (node == null) return;
+				if (node.Left == null && node.Right == null)
+				{
+					if (node.Count != 0) r.Add((node.L, node.Count));
 					return;
 				}
 				Get(node.Left);
