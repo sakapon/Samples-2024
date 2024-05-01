29b0d2945ee794cb9ceefc5f433fe350d8d4a6a4
Keiho Sakapon <git@saka-pon.net>
2024-05-01 20:41:44 +0900

    Add SparseTable class

---
+++ AlgorithmSample/AlgorithmLib10/Trees/LCAs101/SparseTable.cs
@@ -0,0 +1,69 @@
+﻿
+namespace AlgorithmLib10.Trees.LCAs101
+{
+	public static class Monoid
+	{
+		public static Monoid<int> Int32_Add { get; } = new Monoid<int>((x, y) => x + y);
+		public static Monoid<long> Int64_Add { get; } = new Monoid<long>((x, y) => x + y);
+		public static Monoid<int> Int32_Min { get; } = new Monoid<int>((x, y) => x <= y ? x : y, int.MaxValue);
+		public static Monoid<int> Int32_Max { get; } = new Monoid<int>((x, y) => x >= y ? x : y, int.MinValue);
+
+		public static Monoid<int> Int32_ArgMin(int[] a) => new Monoid<int>((x, y) => a[x] <= a[y] ? x : y);
+
+		public static Monoid<int> Int32_Update { get; } = new Monoid<int>((x, y) => x != int.MinValue ? x : y, int.MinValue);
+	}
+
+	public struct Monoid<T>
+	{
+		public Func<T, T, T> Op;
+		public T Id;
+		public Monoid(Func<T, T, T> op, T id = default(T)) { Op = op; Id = id; }
+	}
+
+	public class SparseTable<TValue>
+	{
+		readonly Func<TValue, TValue, TValue> op;
+		readonly TValue iv;
+		readonly int n;
+		readonly List<TValue[]> values;
+
+		public SparseTable(TValue[] a, Monoid<TValue> monoid)
+		{
+			(op, iv) = (monoid.Op, monoid.Id);
+			n = a.Length;
+			values = new List<TValue[]> { a };
+
+			var p = 1;
+			while (p < n)
+			{
+				var t = values[values.Count - 1];
+				var b = new TValue[n];
+				for (int i = 0; i < n; i++)
+					b[i] = i + p < n ? op(t[i], t[i + p]) : t[i];
+				values.Add(b);
+				p <<= 1;
+			}
+		}
+
+		public TValue this[int l, int r] => Get(l, r);
+		public TValue Get(int l, int r)
+		{
+			if (l < 0) l = 0;
+			if (r > n) r = n;
+
+			var c = r - l;
+			if (c == 0) return iv;
+			if (c == 1) return values[0][l];
+
+			var k = 0;
+			while (true)
+			{
+				if ((1 << ++k) >= c)
+				{
+					--k;
+					return op(values[k][l], values[k][r - (1 << k)]);
+				}
+			}
+		}
+	}
+}
---
+++ AlgorithmSample/OnlineTest10/Trees/LCAs/LCA_ABC014_D.cs
@@ -0,0 +1,56 @@
+﻿using AlgorithmLib10.Trees.LCAs101;
+
+namespace OnlineTest10.Trees.LCAs
+{
+	// Test: https://atcoder.jp/contests/abc014/tasks/abc014_4
+	class LCA_ABC014_D
+	{
+		static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
+		static void Main() => Console.WriteLine(Solve());
+		static object Solve()
+		{
+			var n = int.Parse(Console.ReadLine());
+			var es = Array.ConvertAll(new bool[n - 1], _ => Read2());
+			var qc = int.Parse(Console.ReadLine());
+			var qs = Array.ConvertAll(new bool[qc], _ => Read2());
+
+			var map = Array.ConvertAll(new bool[n + 1], _ => new List<int>());
+			foreach (var (u, v) in es)
+			{
+				map[u].Add(v);
+				map[v].Add(u);
+			}
+
+			var tour = new List<(int depth, int v)>();
+			var depths = new int[n + 1];
+			var first = new int[n + 1];
+			DFS(1, -1, 0);
+
+			var monoid = new Monoid<(int depth, int v)>((x, y) => x.depth <= y.depth ? x : y, (int.MaxValue, -1));
+			var st = new SparseTable<(int depth, int v)>(tour.ToArray(), monoid);
+
+			return string.Join("\n", qs.Select(q =>
+			{
+				var (a, b) = q;
+				var l = first[a];
+				var r = first[b];
+				var (d0, _) = l <= r ? st[l, r + 1] : st[r, l + 1];
+				return depths[a] + depths[b] - 2 * d0 + 1;
+			}));
+
+			void DFS(int v, int pv, int d)
+			{
+				depths[v] = d;
+				first[v] = tour.Count;
+				tour.Add((d, v));
+				foreach (var nv in map[v])
+				{
+					if (nv == pv) continue;
+					DFS(nv, v, d + 1);
+					tour.Add((d, v));
+				}
+			}
+		}
+	}
+}
