d38a9e9136826ca4a679d317ec141034e1207a19
Keiho Sakapon <git@saka-pon.net>
2024-05-03 15:18:46 +0900

    Add parents

--- AlgorithmSample/AlgorithmLib10/Trees/ETs101/ETTree.cs
+++ AlgorithmSample/AlgorithmLib10/Trees/ETs101/ETTree.cs
@@ -21,30 +21,34 @@ namespace AlgorithmLib10.Trees.ETs101
 			this.n = n;
 			map = Array.ConvertAll(new bool[n], _ => new List<int>());
 		}
-		public ETTree(int n, int[][] edges, bool twoWay) : this(n)
-		{
-			foreach (var e in edges) AddEdge(e[0], e[1], twoWay);
-		}
-		public ETTree(int n, (int from, int to)[] edges, bool twoWay) : this(n)
-		{
-			foreach (var (from, to) in edges) AddEdge(from, to, twoWay);
-		}
+		public ETTree(int n, int[][] edges, bool twoWay) : this(n) { AddEdges(edges, twoWay); }
+		public ETTree(int n, (int from, int to)[] edges, bool twoWay) : this(n) { AddEdges(edges, twoWay); }
 
 		public void AddEdge(int from, int to, bool twoWay)
 		{
 			map[from].Add(to);
 			if (twoWay) map[to].Add(from);
 		}
+		public void AddEdges(int[][] edges, bool twoWay)
+		{
+			foreach (var e in edges) AddEdge(e[0], e[1], twoWay);
+		}
+		public void AddEdges((int from, int to)[] edges, bool twoWay)
+		{
+			foreach (var (from, to) in edges) AddEdge(from, to, twoWay);
+		}
 
 		public ETResult Build(int root)
 		{
 			var depths = new int[n];
+			var parents = new int[n];
 			var stepMap = Array.ConvertAll(depths, _ => new List<int>());
 			var tour = new List<int>();
 			Array.Fill(depths, -1);
+			Array.Fill(parents, -1);
 			depths[root] = 0;
 			DFS(root);
-			return new ETResult(depths, Array.ConvertAll(stepMap, l => l.ToArray()), tour.ToArray());
+			return new ETResult(depths, parents, Array.ConvertAll(stepMap, l => l.ToArray()), tour.ToArray());
 
 			void DFS(int v)
 			{
@@ -54,6 +58,7 @@ namespace AlgorithmLib10.Trees.ETs101
 				{
 					if (depths[nv] != -1) continue;
 					depths[nv] = depths[v] + 1;
+					parents[nv] = v;
 					DFS(nv);
 					stepMap[v].Add(tour.Count);
 					tour.Add(v);
@@ -65,12 +70,14 @@ namespace AlgorithmLib10.Trees.ETs101
 	public class ETResult
 	{
 		public int[] Depths { get; }
+		public int[] Parents { get; }
 		public int[][] StepMap { get; }
 		public int[] Tour { get; }
 
-		internal ETResult(int[] depths, int[][] stepMap, int[] tour)
+		internal ETResult(int[] depths, int[] parents, int[][] stepMap, int[] tour)
 		{
 			Depths = depths;
+			Parents = parents;
 			StepMap = stepMap;
 			Tour = tour;
 		}
---
+++ AlgorithmSample/OnlineTest10/Trees/ETs/ET_ABC270_C.cs
@@ -0,0 +1,25 @@
+﻿using AlgorithmLib10.Trees.ETs101;
+
+namespace OnlineTest10.Trees.ETs
+{
+	// Test: https://atcoder.jp/contests/abc270/tasks/abc270_c
+	class ET_ABC270_C
+	{
+		static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		static (int, int, int) Read3() { var a = Read(); return (a[0], a[1], a[2]); }
+		static void Main() => Console.WriteLine(Solve());
+		static object Solve()
+		{
+			var (n, x, y) = Read3();
+			var es = Array.ConvertAll(new bool[n - 1], _ => Read());
+
+			var tree = new ETTree(n + 1, es, true);
+			var et = tree.Build(y);
+
+			var r = new List<int>();
+			for (var v = x; v != -1; v = et.Parents[v])
+				r.Add(v);
+			return string.Join(" ", r);
+		}
+	}
+}
