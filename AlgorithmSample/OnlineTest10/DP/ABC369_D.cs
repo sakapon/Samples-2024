5310f21bb1e3f9a1c798c60a8d0dfc0bc46ed2be
Keiho Sakapon <git@saka-pon.net>
2024-09-02 22:37:07 +0900

    Add test

--- AlgorithmSample/OnlineTest10/DP/ABC369_D.cs
+++ AlgorithmSample/OnlineTest10/DP/ABC369_D.cs
@@ -26,8 +26,25 @@
 			return Math.Max(dp0[n], dp1[n]);
 		}
 
-		// 貰う (メモリ節約)
+		// 配る
 		static long Solve2(int n, long[] a)
+		{
+			var dp0 = new long[n + 1];
+			var dp1 = new long[n + 1];
+			dp1[0] = long.MinValue;
+
+			for (int i = 0; i < n; i++)
+			{
+				dp0[i + 1] = Math.Max(dp0[i + 1], dp0[i]);
+				dp1[i + 1] = Math.Max(dp1[i + 1], dp0[i] + a[i]);
+				dp1[i + 1] = Math.Max(dp1[i + 1], dp1[i]);
+				dp0[i + 1] = Math.Max(dp0[i + 1], dp1[i] + 2 * a[i]);
+			}
+			return Math.Max(dp0[n], dp1[n]);
+		}
+
+		// 貰う (メモリ節約)
+		static long Solve3(int n, long[] a)
 		{
 			var dp0 = 0L;
 			var dp1 = long.MinValue;
