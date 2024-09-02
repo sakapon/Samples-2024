8a8181cab5d4088e623fef563307f0d21d24f4eb
Keiho Sakapon <git@saka-pon.net>
2024-09-02 23:05:27 +0900

    Refactor

--- AlgorithmSample/OnlineTest10/DP/ABC369_D.cs
+++ AlgorithmSample/OnlineTest10/DP/ABC369_D.cs
@@ -46,14 +46,14 @@
 		// 貰う (メモリ節約)
 		static long Solve3(int n, long[] a)
 		{
-			var dp0 = 0L;
-			var dp1 = long.MinValue;
+			var v0 = 0L;
+			var v1 = long.MinValue;
 
 			for (int i = 0; i < n; i++)
 			{
-				(dp0, dp1) = (Math.Max(dp0, dp1 + 2 * a[i]), Math.Max(dp0 + a[i], dp1));
+				(v0, v1) = (Math.Max(v0, v1 + 2 * a[i]), Math.Max(v0 + a[i], v1));
 			}
-			return Math.Max(dp0, dp1);
+			return Math.Max(v0, v1);
 		}
 	}
 }
