b0070d6e3152d92d29d3b41d4eddd4a085f4779e
Keiho Sakapon <git@saka-pon.net>
2024-12-14 13:18:00 +0900

    Calculate pi

--- MathSample/PiWpf/MainWindow.xaml.cs
+++ MathSample/PiWpf/MainWindow.xaml.cs
@@ -11,14 +11,41 @@ using System.Windows.Shapes;
 
 namespace PiWpf
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-    }
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			Loaded += (o, e) => Initialize();
+		}
+
+		decimal pi = 0;
+
+		void Initialize()
+		{
+			Task.Run(() =>
+			{
+				var positive = true;
+				for (int i = 1; ; i += 2, positive ^= true)
+				{
+					if (positive) pi += 4m / i;
+					else pi -= 4m / i;
+				}
+			});
+
+			var piTimer = new System.Timers.Timer(50);
+			piTimer.Elapsed += (o, e) =>
+			{
+				Dispatcher.InvokeAsync(() =>
+				{
+					PiText.Text = pi.ToString();
+				});
+			};
+			piTimer.Start();
+		}
+	}
 }
