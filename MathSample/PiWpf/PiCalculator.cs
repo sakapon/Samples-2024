bbfb5f31e60c3e89df3b001a13feaa2efb59879a
Keiho Sakapon <git@saka-pon.net>
2024-12-15 22:57:07 +0900

    Add model class

---
+++ MathSample/PiWpf/PiCalculator.cs
@@ -0,0 +1,72 @@
+﻿namespace PiWpf
+{
+	public class PiCalculator
+	{
+		public bool IsOn;
+		public decimal Pi_d;
+		public RealNumber Pi;
+
+		public void Stop()
+		{
+			IsOn = false;
+		}
+
+		public void ByLeibniz_d()
+		{
+			IsOn = true;
+			Pi_d = 0;
+			var pos = true;
+			for (int i = 1; IsOn; i += 2, pos ^= true)
+			{
+				var d = 4m / i;
+				if (pos) Pi_d += d;
+				else Pi_d -= d;
+			}
+		}
+
+		public void ByLeibniz()
+		{
+			IsOn = true;
+			Pi = 0;
+			RealNumber n4 = 4;
+			var pos = true;
+			for (int i = 1; IsOn; i += 2, pos ^= true)
+			{
+				var d = n4 / i;
+				if (pos) Pi += d;
+				else Pi -= d;
+			}
+		}
+
+		public void ByMadhava_d()
+		{
+			IsOn = true;
+			var r12 = (decimal)Math.Sqrt(12);
+			Pi_d = r12;
+			var p = 1m;
+			for (int i = 3; IsOn; i += 2)
+			{
+				p *= -3;
+				Pi_d += r12 / (i * p);
+			}
+		}
+
+		public void ByMadhava()
+		{
+			IsOn = true;
+			var r12 = RealNumber.Sqrt(12);
+			Pi = r12;
+			RealNumber p = 1;
+			for (int i = 3; IsOn; i += 2)
+			{
+				p *= -3;
+				Pi += r12 / (i * p);
+			}
+		}
+
+		public void ByRamanujan()
+		{
+			IsOn = true;
+		}
+	}
+}
