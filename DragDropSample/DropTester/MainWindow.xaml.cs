d7433ced0dc6a511c58acf19053d56a3407b3b7e
sakapon <git@saka-pon.net>
2024-12-18 17:22:08 +0900

    Implement features

--- DragDropSample/DropTester/MainWindow.xaml.cs
+++ DragDropSample/DropTester/MainWindow.xaml.cs
@@ -11,14 +11,64 @@ using System.Windows.Shapes;
 
 namespace DropTester
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-    }
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			Drop += (o, e) =>
+			{
+				var d = ToDictionary(e.Data);
+				DataItems.ItemsSource = d;
+			};
+
+			Rect1.DragOver += (o, e) =>
+			{
+				// Effects プロパティで動作を指定します。
+				//e.Effects = DragDropEffects.None;
+			};
+			Rect1.Drop += (o, e) =>
+			{
+			};
+		}
+
+		public static Obj[] ToDictionary(IDataObject data)
+		{
+			var l = new List<Obj>();
+			foreach (var f in data.GetFormats())
+			{
+				var o = new Obj { Key = f };
+				try
+				{
+					o.Value = data.GetData(f);
+				}
+				catch (Exception)
+				{
+				}
+				l.Add(o);
+			}
+			return l.ToArray();
+		}
+	}
+
+	public class Obj
+	{
+		public string Key { get; set; }
+		public object Value { get; set; }
+
+		public string TypeName => Value?.GetType().FullName ?? "None";
+		public string ValueString
+		{
+			get
+			{
+				if (Value is null) return "None";
+				if (Value is string[] vs) return string.Join("\n", vs);
+				return Value.ToString();
+			}
+		}
+	}
 }
