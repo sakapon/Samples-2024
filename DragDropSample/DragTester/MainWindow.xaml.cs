28fda6cd9d30c5dd5b0bc280bccde69df6029426
sakapon <git@saka-pon.net>
2024-12-24 17:51:42 +0900

    Implement features

--- DragDropSample/DragTester/MainWindow.xaml.cs
+++ DragDropSample/DragTester/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.IO;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -7,7 +8,6 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace DragTester
 {
@@ -16,9 +16,42 @@ namespace DragTester
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		const string DirName = "temp";
+		readonly MainViewModel vm;
+
 		public MainWindow()
 		{
 			InitializeComponent();
+
+			vm = (MainViewModel)DataContext;
+
+			Directory.CreateDirectory(DirName);
+
+			DragFileText.MouseLeftButtonDown += (o, e) =>
+			{
+				var files = new[] { CreateTempFile() };
+				var data = new DataObject(DataFormats.FileDrop, files);
+				var result = DragDrop.DoDragDrop((DependencyObject)o, data, DragDropEffects.Copy | DragDropEffects.Move | DragDropEffects.Link);
+				vm.Effects.Value = result.ToString();
+				Array.ForEach(files, File.Delete);
+			};
+
+			DragFileText.MouseRightButtonDown += (o, e) =>
+			{
+				var files = new[] { CreateTempFile() };
+				var data = new DataObject(DataFormats.FileDrop, files);
+				var result = DragDrop.DoDragDrop((DependencyObject)o, data, DragDropEffects.Copy | DragDropEffects.Move | DragDropEffects.Link);
+				vm.Effects.Value = result.ToString();
+				Array.ForEach(files, File.Delete);
+			};
+		}
+
+		static string CreateTempFile()
+		{
+			var fileName = Path.GetRandomFileName();
+			var filePath = Path.GetFullPath(Path.Combine(DirName, fileName));
+			File.WriteAllText(filePath, "");
+			return filePath;
 		}
 	}
 }
