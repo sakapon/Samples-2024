16b0e86a5f1dedf40e77a4f2ccb62f5d23088294
Keiho Sakapon <git@saka-pon.net>
2024-12-25 15:48:58 +0900

    Update layout

--- DragDropSample/DragTester/MainWindow.xaml.cs
+++ DragDropSample/DragTester/MainWindow.xaml.cs
@@ -27,31 +27,28 @@ namespace DragTester
 
 			Directory.CreateDirectory(DirName);
 
-			DragFileText.MouseLeftButtonDown += (o, e) =>
-			{
-				var files = new[] { CreateTempFile() };
-				var data = new DataObject(DataFormats.FileDrop, files);
-				var result = DragDrop.DoDragDrop((DependencyObject)o, data, DragDropEffects.Copy | DragDropEffects.Move | DragDropEffects.Link);
-				vm.Effects.Value = result.ToString();
-				Array.ForEach(files, File.Delete);
-			};
-
-			DragFileText.MouseRightButtonDown += (o, e) =>
-			{
-				var files = new[] { CreateTempFile() };
-				var data = new DataObject(DataFormats.FileDrop, files);
-				var result = DragDrop.DoDragDrop((DependencyObject)o, data, DragDropEffects.Copy | DragDropEffects.Move | DragDropEffects.Link);
-				vm.Effects.Value = result.ToString();
-				Array.ForEach(files, File.Delete);
-			};
-
-			DragTextText.MouseLeftButtonDown += (o, e) =>
+			// 上の階層のコントロールから順にイベントが発生します。
+			// EventArgs オブジェクトは共有されます。
+			DragText.MouseLeftButtonDown += (o, e) =>
 			{
 				var data = CreateDataObject(vm.SelectedDragType.Value);
 				var effects = DDEList.SelectedItems.Cast<DragDropEffects>().Aggregate(DragDropEffects.None, (x, y) => x | y);
 				var result = DragDrop.DoDragDrop((DependencyObject)o, data, effects);
 				vm.Effects.Value = result.ToString();
 				e.Handled = true;
+
+				if (data.ContainsFileDropList() && (result & DragDropEffects.Link) == DragDropEffects.None) Array.ForEach(data.GetFileDropList().Cast<string>().ToArray(), File.Delete);
+			};
+
+			DragText.MouseRightButtonDown += (o, e) =>
+			{
+				var data = CreateDataObject(vm.SelectedDragType.Value);
+				var effects = DDEList.SelectedItems.Cast<DragDropEffects>().Aggregate(DragDropEffects.None, (x, y) => x | y);
+				var result = DragDrop.DoDragDrop((DependencyObject)o, data, effects);
+				vm.Effects.Value = result.ToString();
+				e.Handled = true;
+
+				if (data.ContainsFileDropList() && (result & DragDropEffects.Link) == DragDropEffects.None) Array.ForEach(data.GetFileDropList().Cast<string>().ToArray(), File.Delete);
 			};
 
 			MouseLeftButtonDown += (o, e) =>
@@ -76,19 +73,24 @@ namespace DragTester
 			switch (type)
 			{
 				case DragType.File:
-					break;
+					return new DataObject(DataFormats.FileDrop, new[] { CreateTempFile() });
 				case DragType.Files:
-					break;
+					return new DataObject(DataFormats.FileDrop, new[] { CreateTempFile(), CreateTempFile() });
 				case DragType.Text:
 					return new DataObject(DataFormats.UnicodeText, "あいうえお");
 				case DragType.Tuple:
-					break;
+					// 復元可能な型。
+					return new DataObject(DataFormats.Serializable, ("あいうえお", 123));
 				case DragType.Custom:
-					break;
+					// 任意のキーを利用可能。
+					return new DataObject("DragTester", new MyClass());
 				default:
 					throw new InvalidOperationException();
 			}
-			throw new NotImplementedException();
 		}
+	}
+
+	public class MyClass
+	{
 	}
 }
