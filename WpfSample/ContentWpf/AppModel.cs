9c88775f3c59d5cd1b144ddde527f41f01dfe30d
Keiho Sakapon <git@saka-pon.net>
2025-07-13 23:00:32 +0900

    Show images

--- WpfSample/ContentWpf/AppModel.cs
+++ WpfSample/ContentWpf/AppModel.cs
@@ -1,7 +1,15 @@
-﻿namespace ContentWpf
+﻿using System.IO;
+
+namespace ContentWpf
 {
 	public class AppModel
 	{
 		public bool IsForDesign { get; set; }
+
+		public string ContentImagePath { get; } = @".\Images\slime.png";
+		public string ContentImageFullPath => Path.GetFullPath(ContentImagePath);
+
+		public string NoneImagePath { get; } = @".\Images\menfukuro.png";
+		public string NoneImageFullPath => Path.GetFullPath(NoneImagePath);
 	}
 }
