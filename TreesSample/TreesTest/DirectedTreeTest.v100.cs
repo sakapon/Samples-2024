ac8aba219fdc2328ce182f4d67b39c1b0f7fb63a
sakapon <git@saka-pon.net>
2026-02-25 10:48:54 +0900

    Copy test

---
+++ TreesSample/TreesTest/DirectedTreeTest.v100.cs
@@ -0,0 +1,68 @@
+﻿using TreesLib.v100;
+
+namespace TreesTest.v100
+{
+	[TestClass]
+	public class DirectedTreeTest
+	{
+		[TestMethod]
+		public void GetForm_6_2()
+		{
+			var edges = new[]
+			{
+				(0, 1),
+				(0, 2),
+				(0, 3),
+				(3, 4),
+				(4, 5),
+			};
+
+			Assert.AreEqual("(((()))()())", UndirectedTree.GetFormForVertex(edges, 0));
+			Assert.AreEqual("((((()))()))", UndirectedTree.GetFormForVertex(edges, 1));
+			Assert.AreEqual("((((()))()))", UndirectedTree.GetFormForVertex(edges, 2));
+			Assert.AreEqual("((()())(()))", UndirectedTree.GetFormForVertex(edges, 3));
+			Assert.AreEqual("(((()()))())", UndirectedTree.GetFormForVertex(edges, 4));
+			Assert.AreEqual("((((()()))))", UndirectedTree.GetFormForVertex(edges, 5));
+		}
+
+		[TestMethod]
+		public void GetForm_7()
+		{
+			var edges = new[]
+			{
+				(0, 1),
+				(0, 2),
+				(1, 3),
+				(1, 4),
+				(2, 5),
+				(2, 6),
+			};
+
+			Assert.AreEqual("((()())(()()))", UndirectedTree.GetFormForVertex(edges, 0));
+			Assert.AreEqual("(((()()))()())", UndirectedTree.GetFormForVertex(edges, 1));
+			Assert.AreEqual("(((()()))()())", UndirectedTree.GetFormForVertex(edges, 2));
+			Assert.AreEqual("((((()()))()))", UndirectedTree.GetFormForVertex(edges, 3));
+			Assert.AreEqual("((((()()))()))", UndirectedTree.GetFormForVertex(edges, 4));
+			Assert.AreEqual("((((()()))()))", UndirectedTree.GetFormForVertex(edges, 5));
+			Assert.AreEqual("((((()()))()))", UndirectedTree.GetFormForVertex(edges, 6));
+		}
+
+		[TestMethod]
+		public void Parse_6_2()
+		{
+			CollectionAssert.AreEqual(new[] { (0, 1), (1, 2), (2, 3), (0, 4), (0, 5) }, UndirectedTree.Parse("(((()))()())"));
+			CollectionAssert.AreEqual(new[] { (0, 1), (1, 2), (2, 3), (3, 4), (1, 5) }, UndirectedTree.Parse("((((()))()))"));
+			CollectionAssert.AreEqual(new[] { (0, 1), (1, 2), (1, 3), (0, 4), (4, 5) }, UndirectedTree.Parse("((()())(()))"));
+			CollectionAssert.AreEqual(new[] { (0, 1), (1, 2), (2, 3), (2, 4), (0, 5) }, UndirectedTree.Parse("(((()()))())"));
+			CollectionAssert.AreEqual(new[] { (0, 1), (1, 2), (2, 3), (3, 4), (3, 5) }, UndirectedTree.Parse("((((()()))))"));
+		}
+
+		[TestMethod]
+		public void Parse_7()
+		{
+			CollectionAssert.AreEqual(new[] { (0, 1), (1, 2), (1, 3), (0, 4), (4, 5), (4, 6) }, UndirectedTree.Parse("((()())(()()))"));
+			CollectionAssert.AreEqual(new[] { (0, 1), (1, 2), (2, 3), (2, 4), (0, 5), (0, 6) }, UndirectedTree.Parse("(((()()))()())"));
+			CollectionAssert.AreEqual(new[] { (0, 1), (1, 2), (2, 3), (3, 4), (3, 5), (1, 6) }, UndirectedTree.Parse("((((()()))()))"));
+		}
+	}
+}
