8d21fe45f98aa6593beb07ef1fa12d2ba9698c97
Keiho Sakapon <git@saka-pon.net>
2026-02-26 23:10:52 +0900

    Add test

--- TreesSample/TreesTest/DirectedTreeTest.v100.cs
+++ TreesSample/TreesTest/DirectedTreeTest.v100.cs
@@ -61,9 +61,12 @@ namespace TreesTest.v100
 		[TestMethod]
 		public void Parse_7()
 		{
-			CollectionAssert.AreEqual(new[] { (0, 1), (1, 2), (1, 3), (0, 4), (4, 5), (4, 6) }, UndirectedTree.Parse("((()())(()()))"));
-			CollectionAssert.AreEqual(new[] { (0, 1), (1, 2), (2, 3), (2, 4), (0, 5), (0, 6) }, UndirectedTree.Parse("(((()()))()())"));
-			CollectionAssert.AreEqual(new[] { (0, 1), (1, 2), (2, 3), (3, 4), (3, 5), (1, 6) }, UndirectedTree.Parse("((((()()))()))"));
+			CollectionAssert.AreEqual(new[] { (0, 1), (1, 2), (3, 1), (0, 4), (5, 4), (6, 4) }, DirectedTree.Parse("0(+(+()-())+(-()-()))"));
+			CollectionAssert.AreEqual(new[] { (1, 0), (2, 0), (3, 0), (3, 4), (4, 5), (6, 4) }, DirectedTree.Parse("0(-()-()-(+(+()-())))"));
+			CollectionAssert.AreEqual(new[] { (0, 1), (2, 0), (3, 0), (3, 4), (5, 4), (6, 4) }, DirectedTree.Parse("0(+()-()-(+(-()-())))"));
+			CollectionAssert.AreEqual(new[] { (0, 1), (2, 1), (3, 1), (3, 4), (4, 5), (6, 4) }, DirectedTree.Parse("0(+(-()-(+(+()-()))))"));
+			CollectionAssert.AreEqual(new[] { (0, 1), (1, 2), (3, 1), (3, 4), (5, 4), (6, 4) }, DirectedTree.Parse("0(+(+()-(+(-()-()))))"));
+			CollectionAssert.AreEqual(new[] { (1, 0), (2, 1), (3, 1), (3, 4), (5, 4), (6, 4) }, DirectedTree.Parse("0(-(-()-(+(-()-()))))"));
 		}
 	}
 }
