0b31f566d6bb4e6eeec83b5dea22636b625a3c96
Keiho Sakapon <git@saka-pon.net>
2026-02-27 23:26:35 +0900

    Copy class

---
+++ TreesSample/TreesLib/DirectedTree.v102.cs
@@ -0,0 +1,144 @@
+﻿namespace TreesLib.v102
+{
+	// 辺を根とする文字列表現を実装します。
+	// 中心を葉から求めます。
+	public class DirectedTree
+	{
+		const char UO = '(', UC = ')';
+		static readonly StringComparer FormComparer = StringComparer.Ordinal;
+
+		static List<int>[] ToMap((int u, int v)[] edges)
+		{
+			var n = edges.Length + 1;
+			var map = Array.ConvertAll(new bool[n], _ => new List<int>());
+			foreach (var (u, v) in edges)
+			{
+				map[u].Add(v);
+				map[v].Add(u);
+			}
+			return map;
+		}
+
+		public readonly (int u, int v)[] edges;
+		readonly List<int>[] map;
+
+		public DirectedTree((int u, int v)[] edges)
+		{
+			this.edges = edges;
+			map = ToMap(edges);
+		}
+
+		string GetFormByDFS(int v, int parent)
+		{
+			var l = new List<string>();
+			foreach (var nv in map[v])
+			{
+				if (nv == parent) continue;
+				l.Add(GetFormByDFS(nv, v));
+			}
+			l.Sort(FormComparer);
+			var f = string.Join("", l);
+			return $"{UO}{f}{UC}";
+		}
+
+		public string GetFormForVertex(int root)
+		{
+			return GetFormByDFS(root, -1);
+		}
+
+		string GetFormForEdge(int u, int v)
+		{
+			var f1 = GetFormByDFS(u, v);
+			var f2 = GetFormByDFS(v, u);
+			if (FormComparer.Compare(f1, f2) > 0) (f1, f2) = (f2, f1);
+			return f1 + f2;
+		}
+
+		public string GetFormForEdge(int root)
+		{
+			var (u, v) = edges[root];
+			return GetFormForEdge(u, v);
+		}
+
+		int[] GetCenter()
+		{
+			var n = map.Length;
+
+			var rem = n;
+			var degrees = Array.ConvertAll(map, l => l.Count);
+			var l = new List<int>();
+			var lt = new List<int>();
+
+			for (int v = 0; v < n; ++v)
+				if (degrees[v] == 1) l.Add(v);
+
+			while (rem > 2)
+			{
+				foreach (var v in l)
+				{
+					foreach (var nv in map[v])
+					{
+						if (degrees[nv] == 0) continue;
+						--rem;
+						--degrees[v];
+						if (--degrees[nv] == 1) lt.Add(nv);
+					}
+				}
+				(l, lt) = (lt, l);
+				lt.Clear();
+			}
+			return l.ToArray();
+		}
+
+		public string GetNormalForm()
+		{
+			var center = GetCenter();
+			if (center.Length == 1)
+				return GetFormForVertex(center[0]);
+			else
+				return GetFormForEdge(center[0], center[1]);
+		}
+
+		// form: 標準形とは限りません。
+		public static UndirectedTree Parse(string form)
+		{
+			ArgumentNullException.ThrowIfNull(form);
+
+			var edges = new List<(int, int)>();
+			var roots = new List<int>();
+			var vi = -1;
+			var q = new Stack<int>();
+
+			foreach (var c in form)
+			{
+				switch (c)
+				{
+					case UO:
+						++vi;
+						if (q.Count == 0) roots.Add(vi);
+						else edges.Add((q.Peek(), vi));
+						q.Push(vi);
+						break;
+					case UC:
+						if (q.Count == 0) throw new FormatException();
+						q.Pop();
+						break;
+					default:
+						throw new FormatException();
+				}
+			}
+
+			if (q.Count > 0) throw new FormatException();
+			if (roots.Count == 1)
+			{
+				return new UndirectedTree(edges.ToArray());
+			}
+			if (roots.Count == 2)
+			{
+				edges.Add((roots[0], roots[1]));
+				return new UndirectedTree(edges.ToArray());
+			}
+			throw new FormatException();
+		}
+	}
+}
